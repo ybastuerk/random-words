0555540703593aa3fb90ca190e615a694ed24787
Yusuf Bastürk <ybastuerk@schueler.hakzell.at>
2024-04-24 09:26:13 +0200

    init commit

---
+++ src/Sentence.php
@@ -0,0 +1,46 @@
+<?php
+namespace Ybastuerk\RandomWords;
+
+class Sentence{
+    private string $sentence = '';
+
+    /**
+     * Gets the original sentence
+     *
+     * @return string
+     */
+    public function getSentence()
+    {
+        return $this->sentence;
+    }
+
+    /**
+     * @param string $sentence
+     */
+    private function setSentence($sentence)
+    {
+        $this->sentence = trim($sentence);
+    }
+
+
+    function __construct($sentence){
+
+        //$this->sentence = $sentence;
+    $this->setSentence(($sentence));
+    }
+
+    public function getRandomSentence(): string{
+        $words = explode(' ', $this->sentence);
+
+        // More than one word
+        if (count($words)>0){
+
+            $result = shuffle($words);
+
+            if ($result === true){
+                return implode(' ', $words);
+            }
+        }
+        return $this->sentence;
+    }
+}
